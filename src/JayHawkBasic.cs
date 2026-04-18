85a1e3bf95aaf597dc9c83218c9a44ad0ef75c32
iamkillroy <lucasfrias@mailfence.com>
2026-04-18 10:54:46 -0500

    Working on moveable collision

--- src/JayHawkBasic.cs
+++ src/JayHawkBasic.cs
@@ -1,9 +1,12 @@
 using Godot;
 using System;
+using System.ComponentModel;
 
 public partial class JayHawkBasic : CharacterBody2D 
 {
 	// Called when the node enters the scene tree for the first time.
+	private float fireDelay =- 1;
+	//let's init ours
 	public override void _Ready()
 	{
 		GD.Print("hello");
@@ -13,18 +16,21 @@ public partial class JayHawkBasic : CharacterBody2D
 
 	}
     private void OnBodyEntered(Node2D body)
-    {
-        GD.Print(body.Name + " is in range");
-    }
+	{
+		GD.Print(body.Name);
+		if (body.Name == "Fox")
+		{
+			body.QueueFree();//delete that fox next frame
+		}
+	}
 
     private void OnBodyExited(Node2D body)
-    {
-        GD.Print(body.Name + " left range");
-    }
+	{
+		;//implement later
+	}
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		//we have to check the range 
-		//let's check our body collision
+		fireDelay -= (float) delta;
 	}
 }
---
+++ src/StupidTestFox.cs
@@ -0,0 +1,17 @@
+using Godot;
+using System;
+using System.Numerics;
+
+public partial class StupidTestFox : AnimatableBody2D 
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		Position += new Godot.Vector2(-(float) delta*100, 0);
+	}
+}
